9c3500da00d866c3948928f1440e359e970a1ae2
PILAR <PILAR@DESKTOP-SVDSUUQ>
2021-10-12 20:39:48 -0400

    Agregar archivos de proyecto.

---
+++ PersonaAPI/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonaAPI.Models
+{
+    public enum SexType
+    {
+        fem = 0,
+        masc = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "Porfavor ingrese entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre de la persona")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+
+        [Display(Name = "Pasa tiempo")]
+        public string Hobbie { get; set; }
+    }
+}
---
+++ Personaweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Personaweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
